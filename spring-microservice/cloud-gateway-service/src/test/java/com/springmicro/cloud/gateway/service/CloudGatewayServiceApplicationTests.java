a57108552bb75837ac20fa13432ae7e3cc5b288f
Navnath Damale <navnathdamale@gmail.com>
2021-02-02 09:32:37 +0530

    Added API Gateway module for User service and department service

---
+++ spring-microservice/cloud-gateway-service/src/test/java/com/springmicro/cloud/gateway/service/CloudGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmicro.cloud.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
